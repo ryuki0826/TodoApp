3b0487ec9a8f3bf439203537a73a2b0a72b26dc3
Ryu <ryuki.sato.lol@gmail.com>
2020-03-06 08:06:38 +0000

    Add_Validation_To_USer&Add_Login_Upto_p157

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,9 +12,14 @@ class TasksController < ApplicationController
   end
 
   def create
-    task=Task.new(task_params)
-    task.save!
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました"
+    
+    @task=Task.new(task_params)
+    
+    if @task.save
+      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました"
+    else
+      render :new
+    end
   end
   
   def edit
@@ -32,6 +37,7 @@ class TasksController < ApplicationController
     task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました"
   end
+  
   private
   def task_params
     params.require(:task).permit(:name, :description)
---
+++ db/migrate/20200306053301_change_tasks_name_to_null.rb
@@ -0,0 +1,5 @@
+class ChangeTasksNameToNull < ActiveRecord::Migration[5.1]
+  def change
+    change_column_null :tasks, :name, false, ""
+  end
+end
